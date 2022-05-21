4403ddb3867eb0ec56ee9b48c64373c48ab1f930
Oleg O <oleg_biblyi@epam.com>
2022-05-21 14:09:19 +0300

    part 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private renderer: Renderer2
+  ) {}
+
+  onSetThemeOne() {
+    this.renderer.removeAttribute(this.document.body, 'class');
+  }
+
+  onSetThemeTwo() {
+    this.renderer.setAttribute(this.document.body, 'class', 'theme-two');
+  }
+}
